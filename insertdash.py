6731e0cba11d5eab93b4ec68320673ea7e0204c5
fierlion <fierlion@gmail.com>
2016-02-20 10:07:14 -0800

    insertdash for all odds, and paired odds

---
+++ insertdash.py
@@ -0,0 +1,20 @@
+import itertools
+
+def insert_all_dash(num_in):
+    num_list = list(str(num_in))
+    odd_indices = [(idx + '-') if int(idx) % 2 != 0 else idx for idx in num_list]
+    joined = "".join(odd_indices)
+    return joined if joined[-1] != '-' else joined[:-1]
+
+def insert_dash(num):
+    num_list = list(str(num)+'0')
+    a, b = itertools.tee(num_list)
+    next(b, None) # offset b by one
+    zipped = itertools.izip(a, b)
+    odd_dashes = [(a+'-') if int(a)%2 != 0 and int(b)%2 != 0 else a for a,b in zipped]
+    return "".join(odd_dashes)
+
+if __name__=='__main__':
+    print(insert_dash(454793))
+    print(insert_dash(2468))
+    print(insert_dash(1357))
