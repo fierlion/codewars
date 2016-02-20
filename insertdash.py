42440faa5665a63172400af656de96210b8cde63
fierlion <fierlion@gmail.com>
2016-02-20 10:10:30 -0800

    added comments

--- insertdash.py
+++ insertdash.py
@@ -1,12 +1,15 @@
 import itertools
 
 def insert_all_dash(num_in):
+    """inserts dash after all odd numbers;
+    trims the final dash if the last number is odd"""
     num_list = list(str(num_in))
     odd_indices = [(idx + '-') if int(idx) % 2 != 0 else idx for idx in num_list]
     joined = "".join(odd_indices)
     return joined if joined[-1] != '-' else joined[:-1]
 
 def insert_dash(num):
+    """inserts dash between pairs of odd numbers"""
     num_list = list(str(num)+'0')
     a, b = itertools.tee(num_list)
     next(b, None) # offset b by one
